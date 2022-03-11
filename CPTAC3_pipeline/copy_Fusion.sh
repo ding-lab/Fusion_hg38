e018bb7f2396c27d92ec3e8bbe503a32dd74fa97
EJ Song <80489022+sscien@users.noreply.github.com>
2022-03-11 16:06:18 -0600

    Update copy_Fusion.sh

--- CPTAC3_pipeline/copy_Fusion.sh
+++ CPTAC3_pipeline/copy_Fusion.sh
@@ -6,5 +6,4 @@ target=$2
 ls $dir | grep 'C' | while read sample
 do
         cp $dir/$sample/Merged_Fusions/Filtered_Fusions_in_${sample}.tsv $2/${sample}.Fusions.txt
-        #awk -v x=${sample} '{print $0, x}' $dir/$sample/${sample}_somatic > $2/${sample}.sites.tsv
 done
